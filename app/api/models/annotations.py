9fab73f5739e9c5129f9416a4c589f524a392b50
kemo <kemo3855@yahoo.com>
2024-06-18 14:51:11 +0800

    pydantic error

--- app/api/models/annotations.py
+++ app/api/models/annotations.py
@@ -4,9 +4,11 @@ from typing import Optional, Dict
 # from app.api.models.users import UserResponse
 
 class AnnotationBase(BaseModel):
-    annotation_type: str
-    annotation_data: dict
-    timecode: str
+    # annotation_type: str
+    # annotation_data: dict
+    annotation_data: Optional[Dict[str, str]] = None  # Adjust types as necessary
+    annotation_type: Optional[str] = None
+    timecode: Optional[str] = None
 
 class AnnotationCreate(AnnotationBase):
     pass
@@ -26,13 +28,13 @@ class UserResponse(BaseModel):
 
     class Config:
         orm_mode = True    
-class AnnotationResponse(BaseModel):
+class AnnotationResponse( AnnotationBase):
     id: int
     video_id: int
     user: UserResponse  # Ensure `user` is correctly populated as `UserResponse`
-    annotation_data: Optional[Dict[str, str]] = None  # Adjust types as necessary
-    annotation_type: Optional[str] = None
-    timecode: Optional[str] = None
+    # annotation_data: Optional[Dict[str, str]] = None  # Adjust types as necessary
+    # annotation_type: Optional[str] = None
+    # timecode: Optional[str] = None
     created_at: datetime
     updated_at: datetime
 
